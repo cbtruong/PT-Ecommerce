20aa87cedc9db58df7c0271ca3c62e76a826001a
cbtruong <batruong12122002@gmail.com>
2025-07-30 05:16:16 +0700

    create router functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Collection from './pages/Collection.jsx';
+import PlaceOrder from './pages/PlaceOrder.jsx';
+import Product from './pages/Product.jsx';
+import Cart from './pages/Cart.jsx';
+import Orders from './pages/Orders.jsx';
+import Login from './pages/Login.jsx';
+import Navbar from './components/Navbar.jsx';
+
+
 
 const App = () => {
   return (
-    <div className='text-red-400'>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
+    </div>
   )
 }
 
